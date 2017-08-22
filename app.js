1b605c9d1ec71004158a6c67c6b98371d4f1c216
JadeMyles <jade.myles@yahoo.com>
2017-08-22 13:51:11 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+var config = {
+    apiKey: "AIzaSyCZ7chAeB6bz4l6ZONqHTaviTK4h4sX7A4",
+    authDomain: "edm-database.firebaseapp.com",
+    databaseURL: "https://edm-database.firebaseio.com",
+    projectId: "edm-database",
+    storageBucket: "",
+    messagingSenderId: "385705953457"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
 $("#button_submit").on("click", function() {
-    alert("submit clicked");
+
+    var name = $("#name").val();
+    var role = $("#role").val();
+    var startDate = $("#startDate").val();
+    var monthlyRate = $("#monthlyRate").val();
+
+	database.ref().push({
+		"name" : name,
+		"role" : role,
+		"startDate" : startDate,
+		"monthlyRate" : monthlyRate
+	});
+
 });
+
+var lastEmployee = function()
